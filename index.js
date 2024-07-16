5b8f8ba96d4e11d87fe44b103827643fe0555d44
Hariprasath148 <janhharigithub@gmail.com>
2024-07-16 11:46:07 +0530

    major

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+window.addEventListener("DOMContentLoaded",()=>{
+    window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth' // Optional: 'auto' or 'smooth'
+    });
+});
+
 let height=(window.innerHeight*6)/9;
 let change_top_height = 0;
 let change_bottom_height = height;
@@ -6,22 +14,37 @@ let slide_name_backward = ["rev-one","rev-two","rev-three","rev-four","rev-five"
 let position = 0;
 window.addEventListener('scroll', function(e) {
     if(this.window.scrollY > change_bottom_height){
+        if(position == 8){
+            return;
+        }
+        this.document.querySelector("body").style.overflowY ="hidden";
         position++;
         document.querySelector(".main-two").classList.add(slide_name_forward[position]);
         remove_reverse_name();
+        document.querySelector(".main-two").addEventListener("animationend",()=>{
+            this.document.querySelector("body").style.overflowY ="scroll";
+        });
         change_top_height+=height;
         change_bottom_height+=height;
         console.log(change_bottom_height+"    "+change_top_height+"     "+position);
     }
     if(this.window.scrollY < change_top_height){
+        if(position < 0){
+            return;
+        }
+        this.document.querySelector("body").style.overflowY ="hidden";
         document.querySelector(".main-two").classList.remove(slide_name_forward[position]);
         remove_reverse_name();
         position--;
         document.querySelector(".main-two").classList.add(slide_name_backward[position]);
+        document.querySelector(".main-two").addEventListener("animationend",()=>{
+            this.document.querySelector("body").style.overflowY ="scroll";
+        });
         change_top_height-=height;
         change_bottom_height-=height;
         console.log(change_bottom_height+"    "+change_top_height+"     "+position);
     }
+
 });
 
 function remove_reverse_name(){
